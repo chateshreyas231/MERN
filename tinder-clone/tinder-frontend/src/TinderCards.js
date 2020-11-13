2dee352814981a794ff33a2c6014e6d715ac08b3
Shreyas Chate <chateshreyas231@gmail.com>
2020-11-13 14:46:10 +0530

    first commit

---
+++ tinder-clone/tinder-frontend/src/TinderCards.js
@@ -0,0 +1,59 @@
+import TinderCard from 'react-tinder-card';
+import React, { useState,useEffect } from 'react';
+import './TinderCards.css';
+import axios from './axios';
+
+function TinderCards ()  {
+    const [people, setPeople]=useState([
+        {"name":"Elon Musk",
+        "imgUrl":"https://upload.wikimedia.org/wikipedia/commons/e/ed/Elon_Musk_Royal_Society.jpg"
+        }
+    ]);
+
+    useEffect(() => {
+        async function fetchData(){
+            const req = await axios.get("/tinder/cards");
+
+            setPeople(req.data);
+        }
+
+        fetchData();
+    }, []);
+    
+
+
+    const swiped=(direction, nameToDelete) => {
+        console.log("removing: "+nameToDelete);
+        //setLastDirection(direction);
+    };
+
+    const outOfFrame =(name) => {
+        console.log(name + "left the screen!");
+    };
+
+    return (
+            <div className="tinderCards">   
+            <div className="tinderCards__cardContainer ">
+             {people.map( (person =>(
+            <TinderCard
+                className="swipe"
+                key={person.name}
+                preventSwipe={"up", "down"}
+                onSwipe={(dir) => swiped(dir, person.name)}
+                onCardLeftScreen={() => outOfFrame(person.name)}
+            >
+            <div
+             style={{backgroundImage: `url(${person.imgUrl})`}}
+             className="card"
+             >
+                 <h3>{person.name}</h3>
+             </div>
+
+            </TinderCard>
+             )))}       
+        </div>
+    </div>
+  );
+}
+
+export default TinderCards
